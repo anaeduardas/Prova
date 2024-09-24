812833321ae5178d15e7d5e25ceae18a9c9d2867
Ana Eduarda <anasantoseduarda296@gmail.com>
2024-09-23 22:25:18 -0300

    22.25

---
+++ exercicio4.js
@@ -0,0 +1,33 @@
+/*4) Apresente o Menu de Lanches:
+1 -> X-SALADA-> R$ 20,00
+2 -> X-BACON-> R$ 25,00
+3 -> X-TUDO-> R$ 23,00
+4 -> BAURU -> R$ 29,99
+5 -> AMERICANO -> 22,50
+
+Apresente o Menu de Bebidas:
+Refrigerante Lata -> R$ 5,00
+Refrigerante 2L -> R$ 10,00
+Agua -> R$ 3,50
+Suco -> R$ 7,50
+
+Solicite ao usuário qual Pizza e quantidade que deseja comprar, qual refrigerante e quantidade, ao final apresente as formas de pagamento:
+PIX - 10% de desconto.
+Dinheiro - 5% de desconto.
+Débito - 3% de desconto.
+Crédito - sem desconto.
+Apresente todos os dados da compras, com seus calculos e valores;
+*/
+
+var menuLanche = "===== menu lanches ====="
+menuLanche = menuLanche = "cód | nome | preço \n"
+menuLanche += "1 -> X-SALADA-> R$ 20,00\n "
+menuLanche += "2 -> X-BACON-> R$ 25,00\n "
+menuLanche += "3 -> X-TUDO-> R$ 23,00\n "
+menuLanche += "4 -> BAURU -> R$ 29,99\n "
+menuLanche += "5 -> AMERICANO -> 22,50\n "
+
+var lanches = parseInt(prompt(menuLanche));
+var quantidade = parseInt(prompt("Digite a quantidade: "));
+
+if ()
