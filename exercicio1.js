625b7f720f8f3809af94ecddbc01a08c1c1f81f8
Ana Eduarda <anasantoseduarda296@gmail.com>
2024-09-26 22:21:53 -0300

    Update exercicio1.js

--- exercicio1.js
+++ exercicio1.js
@@ -7,26 +7,26 @@ de R$ 33.919,81 a R$ 45.012,60 (15%)
 e acima (22,5%).
  Apresente o valor digitado e o valor do imposto a ser pago.
 */
-
-var rendaAnual = prompt("Digite sua Renda Anual: ");
+var rendaAnual;
+var rendaAnual = parseFloat(prompt("Digite sua Renda Anual: "));
 var imposto;
 
-if (rendaAnual == 22.847,76){
-    console.log ("isento");
+if (rendaAnual <= 0){
+    
 }
-    else if (rendaAnual >= 33.919){
-        imposto = rendaAnual * 0.075;
-        console.log("Renda Anual: R$ " + rendaAnual);
-        console.log("Imposto a pagar: R$ " + imposto);
-    }
-        else if (rendaAnual >= 45.012){
-            imposto = rendaAnual * 0.15;
-            console.log("Renda Anual: R$ " + rendaAnual);
-            console.log("Imposto a pagar: R$ " + imposto);
-        }
-            else if (rendaAnual >= 45.012){
-                imposto = rendaAnual *  0.225;
-                console.log("Renda Anual: R$ " + rendaAnual);
-                console.log("Imposto a pagar: R$ " + imposto);
-            }
-
+if (rendaAnual < 22.847){
+    imposto = 0;
+    console.log("isento!");
+} 
+else if (rendaAnual >= 33.919,80){
+    imposto = rendaAnual * 0.075;
+    console.log("o valor digitado: "  + rendaAnual + " // Valor do imposto é: "   + imposto );
+}
+else if (rendaAnual >= 45.012){
+    imposto = rendaAnual * 0.15;
+    console.log("o valor digitado: "  + rendaAnual + " // Valor do imposto é: "   + imposto );
+}
+else if (rendaAnual >= 45.013){
+    imposto = rendaAnual * 0,225;
+    console.log("o valor digitado: "  + rendaAnual + " // Valor do imposto é: "   + imposto );
+}
